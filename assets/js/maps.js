99ea95f7840dd71087fb6a021e4d646ddf2a4b33
Amzzy78 <murphy.annmarie78@gmail.com>
2022-03-21 18:39:54 +0000

    Add google maps with markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,25 +1,37 @@
-// const properties = {"lat": 12, "lng": 100}
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
 
-// /** DECLARE A FUNCTION TO CREATE THE MAP */
-// function myMap() {
-//     /** THE mapProp VARIABLE DEFINES THE PROPERTIES OF THE MAP */    
-//     const mapProp = {
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-//     /** THE CENTER PROPERTY SPECIFIES WHERE TO CENTER THE MAP
-//      * HERE WE USE THE .LatLing CONSTRUCTORS FROM THE MAP CLASS
-//      */
-//     // center: new google.maps.LatLing(53, 5),
-//     center: properties,
+    var locations = [{
+        lat: 40.785091,
+        lng: -73.9685
+        },
+        {
+            lat: 41.084045,
+            lng: -73.874245
+        },
+        {
+            lat: 40.754932,
+            lng: -73.984016
+        },
 
-//     /** zoom SET THE ZOOM LEVEL OF THE MAP */
-//     zoom: 5,
-// };
-// /** CREATES A NEW MAP INSIDE THE DIV THAT HAS THE ID OF "map" SET
-//  * IT THEN CALLS THE PROPERTIES TO SEE HOW TO RENDER THE MAP
-//  */
-// const map = new google.maps.Map(document.getElementById("map"), mapProp);
+    ];
 
-// }
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
 
-// /** CALL MAP FUNCTION */
-// myMap()
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
+}
