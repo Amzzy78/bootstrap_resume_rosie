8fcce4a730bf630cba13e1549fc17e82edb42b69
Amzzy78 <murphy.annmarie78@gmail.com>
2022-03-21 14:34:38 +0000

    Troubleshoot succcessful on api maps issue

---
+++ assets/js/maps.js
@@ -0,0 +1,25 @@
+// const properties = {"lat": 12, "lng": 100}
+
+// /** DECLARE A FUNCTION TO CREATE THE MAP */
+// function myMap() {
+//     /** THE mapProp VARIABLE DEFINES THE PROPERTIES OF THE MAP */    
+//     const mapProp = {
+
+//     /** THE CENTER PROPERTY SPECIFIES WHERE TO CENTER THE MAP
+//      * HERE WE USE THE .LatLing CONSTRUCTORS FROM THE MAP CLASS
+//      */
+//     // center: new google.maps.LatLing(53, 5),
+//     center: properties,
+
+//     /** zoom SET THE ZOOM LEVEL OF THE MAP */
+//     zoom: 5,
+// };
+// /** CREATES A NEW MAP INSIDE THE DIV THAT HAS THE ID OF "map" SET
+//  * IT THEN CALLS THE PROPERTIES TO SEE HOW TO RENDER THE MAP
+//  */
+// const map = new google.maps.Map(document.getElementById("map"), mapProp);
+
+// }
+
+// /** CALL MAP FUNCTION */
+// myMap()
